52bf02fcd02fecdb2bc0267cfd7f861f8b2f4ae3
kavya-praneetha-v <kavyapv1@umbc.edu>
2025-03-05 09:47:31 -0500

    [1780] [Time Beats: 100.00%] [Memory Beats: 18.43%] - LeetPush

---
+++ Check-if-Number-is-a-Sum-of-Powers-of-Three.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        power = 1
+        while power * 3 <= n:
+            power *= 3
+        
+        while n > 0 and power > 0:
+            if n >= power:
+                n -= power 
+            power //= 3  
+        
+        return n == 0
