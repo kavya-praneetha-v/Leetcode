c5dbc72db3f766826edd6ad8f3f9ae8e76c37f36
kavya-praneetha-v <kavyapv1@umbc.edu>
2025-03-28 15:02:56 -0400

    [1280] [Time Beats: 91.95%] - LeetPush

---
+++ Students-and-Examinations.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select s.student_id, s.student_name, su.subject_name, count(e.student_id) attended_exams
+from Students s
+cross join Subjects Su
+left join Examinations e
+on s.student_id = e.student_id
+and su.subject_name = e.subject_name
+
+group by s.student_id, su.subject_name
+order by s.student_id, su.subject_name
