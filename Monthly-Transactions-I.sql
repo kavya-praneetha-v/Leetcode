d4662ec95084873d8ab70415a067ed1f81e34a5a
kavya-praneetha-v <kavyapv1@umbc.edu>
2025-03-31 14:59:05 -0400

    [1193] [Time Beats: 83.84%] - LeetPush

---
+++ Monthly-Transactions-I.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select left(trans_date,7) as month, country, count(state) as trans_count, sum(state = 'approved')  as approved_count,
+sum(amount) as trans_total_amount,  sum((state = 'approved') * amount) as approved_total_amount
+from Transactions
+group by month, country
